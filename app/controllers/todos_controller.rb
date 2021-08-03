8512581d645594a4c4ca583a15a913f79832dbb3
Martin Cervantes <cervantes.martine@gmail.com>
2021-08-03 17:57:26 -0600

    Add new and create method

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,23 @@
 class TodosController < ApplicationController
   def index
+    @todos = Todo.all
+  end
+
+  def new
+    @todo = Todo.new
+  end
+
+  def create
+    @todo = Todo.create(todo_params)
+
+    if @todo.save
+      redirect_to todos_path
+    end
+end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:title)
   end
 end
